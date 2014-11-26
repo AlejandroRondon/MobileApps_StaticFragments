0056b939f97e1e58b43884c304023d26404fc5d2
AlejandroRondon <rondon_udea@hotmail.com>
2014-11-26 15:51:25 -0500

    Version 0 Intents

---
+++ StaticFragments/gen/com/Blackrondonapps/staticfragments/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.Blackrondonapps.staticfragments;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
